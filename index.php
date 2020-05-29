5cbabb4a9556b5e017cc475c64d230926e08e8dd
Vadim Balan <vbalan@mail.greenriver.edu>
2020-05-29 00:15:49 -0400

    Added all the pages needed and finished the car and home page

--- index.php
+++ index.php
@@ -33,13 +33,49 @@ $f3->route('GET|POST /information', function ($f3)
     echo $view->render('views/information.html');
 });
 
-// Basic information page
-$f3->route('GET|POST /car', function ()
+// Basic information about car page
+$f3->route('GET|POST /car', function ($f3)
 {
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        $f3->reroute('/exterior');
+    }
 
     $view = new Template();
     echo $view->render('views/car.html');
 });
+
+// Information about exterior of car
+$f3->route('GET|POST /exterior', function ($f3)
+{
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        $f3->reroute('/interior');
+    }
+
+    $view = new Template();
+    echo $view->render('views/exterior.html');
+});
+
+// Information about interior of car
+$f3->route('GET|POST /interior', function ($f3)
+{
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        $f3->reroute('/interior');
+    }
+
+    $view = new Template();
+    echo $view->render('views/interior.html');
+});
+
+// Summary page of the final product
+$f3->route('GET /summary', function ($f3)
+{
+    $view = new Template();
+    echo $view->render('views/summary.html');
+});
+
 
 // Run F3
 $f3->run();
